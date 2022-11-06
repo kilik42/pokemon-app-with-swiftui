020bc150d9640ba1c2d2e088eb0f4f08378111c4
marvin evins <marvinevins@yahoo.com>
2022-11-06 15:45:37 -0600

    working on pokemon app in swiftui

--- pokemonSwiftUi/ContentView.swift
+++ pokemonSwiftUi/ContentView.swift
@@ -8,14 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var creatures = ["pikachu", "squirtle","charzard"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack{
+            List(creatures, id: \.self){ creature in
+                Text(creature)
+                    .font(.title2)
+                
+            }
+            .listStyle(.plain)
         }
-        .padding()
     }
 }
 
