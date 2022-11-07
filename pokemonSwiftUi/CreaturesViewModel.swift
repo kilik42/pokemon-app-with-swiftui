abaf6853ec2f24d7f91bc10f487d7023511ca368
marvin evins <marvinevins@yahoo.com>
2022-11-06 20:47:10 -0600

    working on the creature loop

--- pokemonSwiftUi/CreaturesViewModel.swift
+++ pokemonSwiftUi/CreaturesViewModel.swift
@@ -12,4 +12,27 @@ import Foundation
 
 class CreaturesViewModel: ObservableObject{
     
+    private struct Returned : Codable{
+        var count:Int
+        var next: String
+    }
+    var urlString = "https://pokeapi.co/api/v2/pokemon/"
+    func getData() async {
+        
+        guard let url = URL(string: urlString) else{
+            print("error: could not create a url from \(urlString) ")
+            return
+        }
+        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            guard let returned = try? JSONDecoder().decode(Returned.self, from: data) else{
+                return
+            }
+        }catch{
+            print("Error: could notget data from \(urlString)")
+        }
+    }
+    
 }
